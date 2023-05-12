fe79ebfc22575d9aa7d15430a393fc912956b35c
kikegal <kikedosul@gmail.com>
2023-05-12 14:04:29 -0300

    adicionei chaves por segunda vez

--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
+    keys: ['myKeyA', 'myKeysB']
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
