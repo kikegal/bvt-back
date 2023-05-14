04221a04a98452bfb8f270ad926ea083d380d997
kikegal <kikedosul@gmail.com>
2023-05-14 11:53:19 -0300

    save key env

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET','70bfcc011b2ffdb8757730921aa6c4a1'),
+    secret: env('ADMIN_JWT_SECRET','7b8a576d9a2f52dffba78090f89205c3'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
