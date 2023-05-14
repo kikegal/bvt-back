1e06e8fa71c8c679899d8e1336cbaa6bff4e6726
kikegal <kikedosul@gmail.com>
2023-05-14 10:41:07 -0300

    adicionei jwt a render

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,7 @@
 module.exports = ({ env }) => ({
     
-    plugins: {
-      "users-permissions": {
-        jwtSecret: env('JWT_SECRET', 'rxJ6AXusPGXoUGgBLQW1Lg=='),
-        
-      },
+  "users-permissions": {
+    jwtSecret: env("JWT_SECRET"),
       
     },
     
